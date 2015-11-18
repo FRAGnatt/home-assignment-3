dcbf353192f5e905d3871c7dbdc185e4cdbdbb5c
Zabrodin Konstantin <fragnatt@gmail.com>
2015-11-18 20:06:01 +0300

    CompleteSecond

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -15,21 +15,35 @@ class EvaluateTestCase(unittest.TestCase):
 			doIt("not_supported", 1, 2)
 
 	def test_supported_method(self):		
-		self.assertEqual(doIt("+", 1, 2), 1 + 2)
+		self.assertEqual(doIt("+", 9, 22), 31)
 
 class OperatorsTestCase(unittest.TestCase):
 
 	def test_add(self):
-		self.assertEqual(doIt('+' , 2, 3.5), 2 + 3.5)
+		self.assertEqual(doIt('+' , 6, 8), 14)
+	def test_addString(self):
+		self.assertEqual(doIt('+' , '6', '8'), 14)
 
 	def test_sub(self):
-		self.assertEqual(doIt('-', 2, 3.5), 2 - 3.5)
+		self.assertEqual(doIt('-', 2, 3.5), -1.5)
+
+	def test_subString(self):
+		self.assertEqual(doIt('-', '2', '3.5'), -1.5)
+	
+	def test_sub2(self):
+		self.assertEqual(doIt('-', 10, 2), 8)
 
 	def test_mul(self):
 		self.assertEqual(doIt('*', 2, 3.5), 2 * 3.5)
 
+	def test_mulString(self):
+		self.assertEqual(doIt('*', '2', '3.5'), 2 * 3.5)
+
 	def test_div(self):
-		self.assertEqual(doIt('/', 4, 2), 4 / 2)
+		self.assertEqual(doIt('/', 10, 5), 2)
+
+	def test_div2(self):
+		self.assertEqual(doIt('/', 5, 2), 2.5)
 
 	def test_sin(self):
 		self.assertEqual(doIt('sin', 2), sin(2))
